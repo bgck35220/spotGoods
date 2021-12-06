c51f712fb7c43aedf081142d91835bce8c5eccd2
Miru <hungche0701@gmail.com>
2021-12-06 15:54:47 +0800

    商品頁面(還沒連上SQL)

---
+++ store/js/storeScript.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        450: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
