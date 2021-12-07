bd024dfaa04a5770fe1e08f6cf39f44c08f7d7e2
chang <ch96963@gmail.com>
2021-12-07 20:40:02 +0800

    訂單總覽 ok

--- admin/order/orders-list.php
+++ admin/order/orders-list.php
@@ -4,61 +4,168 @@ if (!isset($_SESSION["user"])) {
     header("location:admin-Login.php");
 };
 
-// $sqlSellers="SELECT * FROM
-//  ((order_all AS orderA INNER JOIN users AS usersB ON orderA.users_id=usersB.id) 
-//  INNER JOIN products AS productsC ON orderA.products_id=productsC.id) 
-//  INNER JOIN sellers AS sellersD ON orderA.sellers_id=sellersD.id 
-//  WHERE orderA.id = 1" ;
 
-// $sqlSellers="SELECT producs.name,sellers.name,users.name ,FROM oreder_all.id , sellers.id , WHERE order_all.id = sellers.id";
+$sqlTotal = 
+"SELECT a.* ,b.id as users_id,
+b.account as user_account,
+b.name as user_name,
+b.email as user_email,
+b.address as user_address,
+b.created_at as user_created_at,
+b.valid as user_valid,
+c.id as seller_id,
+c.account as seller_account,
+c.email as seller_email,
+c.name as seller_name,
+c.created_at as seller_created_at,
+d.name as products_name,
+d.valid as products_valid,
+d.created_at as products_created_at
+FROM order_all AS a
+JOIN users as b on a.users_id = b.id
+JOIN sellers as c on a.sellers_id = c.id
+JOIN products as d on a.products_id = d.id
+";
+$stmtTotal = $db_host->prepare($sqlTotal);
+try {
+    $stmtTotal->execute();
+    $totalUsersCount = $stmtTotal->rowCount();
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
-// $sqlSellers="SELECT 訂單.name,商家.name,客戶.name ,FROM 訂單.id , 客戶.id WHERE 訂單.id = 客戶.id";
-
-//會員資料彈跳式窗
+// 會員資料彈跳式窗
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    $sqlUsreTable = "SELECT a.id , 
-    b.name as 'users_id', 
-    c.id as 'sellers_id' ,
-    c.name as'sellers_name',
-    d.id as 'products_id' , 
-    d.price as 'order_price' ,
-    d.name as 'products_name', 
-    a.order_time , a.status FROM 
-    `order_all` a , `users` b , `sellers` c , `products` d 
-    where a.id = b.id and a.sellers_id = c.id and a.products_id = d.id AND a.id=?";
+    $sqlUsreTable = "SELECT a.* ,b.id as users_id,
+    b.account as user_account,
+    b.name as user_name,
+    b.email as user_email,
+    b.address as user_address,
+    b.created_at as user_created_at,
+    b.valid as user_valid,
+    c.id as seller_id,
+    c.account as seller_account,
+    c.email as seller_email,
+    c.name as seller_name,
+    c.created_at as seller_created_at,
+    d.name as products_name,
+    d.valid as products_valid,
+    d.created_at as products_created_at
+    FROM order_all AS a
+    JOIN users as b on a.users_id = b.id
+    JOIN sellers as c on a.sellers_id = c.id
+    JOIN products as d on a.products_id = d.id
+    WHERE a.id = ?";
     $stmtUserTable = $db_host->prepare($sqlUsreTable);
 
     try {
         $stmtUserTable->execute([$id]);
         $rowUserUserTable = $stmtUserTable->fetch();
-        $userExistUserTable = $stmtUserTable->rowCount();
+      
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
-};
+
+
+}
 
 
 
-$sqlOrder = "SELECT a.id , 
-b.name as 'users_id', 
-c.id as 'sellers_id' ,
-c.name as'sellers_name',
-d.id as 'products_id' , 
-d.price as 'order_price' ,
-d.name as 'products_name', 
-a.order_time , a.status FROM 
-`order_all` a , `users` b , `sellers` c , `products` d 
-where a.id = b.id and a.sellers_id = c.id and a.products_id = d.id; ";
+if (isset($_GET['search'])) {
+    $search = $_GET['search'];
+    $sqlOrder = "SELECT a.* ,b.id as users_id,
+    b.account as user_account,
+    b.name as user_name,
+    b.email as user_email,
+    b.address as user_address,
+    b.created_at as user_created_at,
+    b.valid as user_valid,
+    c.id as seller_id,
+    c.account as seller_account,
+    c.email as seller_email,
+    c.name as seller_name,
+    c.created_at as seller_created_at,
+    d.name as products_name,
+    d.valid as products_valid,
+    d.created_at as products_created_at
+    FROM order_all AS a
+    JOIN users as b on a.users_id = b.id
+    JOIN sellers as c on a.sellers_id = c.id
+    JOIN products as d on a.products_id = d.id
+    WHERE a.id like '%$search%' OR b.name like '%$search%' OR c.name like '%$search%' ";
 
-// $sqlOrder="SELECT * FROM order_all";
+
+} else{
+ //分頁功能
+ if (isset($_GET['p'])) {
+    $p = $_GET['p'];
+} else {
+    $p = 1;
+}
+
+$pageItems = 10; //一頁6個
+$startItem = ($p - 1) * $pageItems; //求出LIMIT第一個數字
+$pageConet = $totalUsersCount / $pageItems; //總筆數除一頁10個 = 總頁數
+$pageR = $totalUsersCount % $pageItems; // 總筆數除一頁顯示數量 如果有餘數代表他是下一頁
+$starNo = ($p) * $pageItems - 9;
+$starEnd = $pageItems * ($p);
+if ($pageR !== 0) {
+    $pageConet = ceil($pageConet); //總頁數餘數不為0 讓他無條件進位
+
+    if ($pageConet == $p) {
+        $starEnd = $starEnd - ($pageItems - $pageR);
+    }
+}
+        $sqlOrder="SELECT a.* ,b.id as users_id,
+        b.account as user_account,
+        b.name as user_name,
+        b.email as user_email,
+        b.address as user_address,
+        b.created_at as user_created_at,
+        b.valid as user_valid,
+        c.id as seller_id,
+        c.account as seller_account,
+        c.email as seller_email,
+        c.name as seller_name,
+        c.created_at as seller_created_at,
+        d.name as products_name,
+        d.valid as products_valid,
+        d.created_at as products_created_at
+        FROM order_all AS a
+        JOIN users as b on a.users_id = b.id
+        JOIN sellers as c on a.sellers_id = c.id
+        JOIN products as d on a.products_id = d.id
+        ORDER BY id";
+
+
+
+}
+
 $stmtOrder= $db_host->prepare($sqlOrder);
 try{ 
     $stmtOrder->execute();
-
+    $totalUsersCount = $stmtOrder->rowCount();
 }catch(PDOException $e){
     echo $e->getMessage();
 }
+
+
+
+
+// $sqlOrder = "SELECT a.id , 
+// b.name as 'users_id', 
+// c.id as 'sellers_id' ,
+// c.name as'sellers_name',
+// d.id as 'products_id' , 
+// d.price as 'order_price' ,
+// d.name as 'products_name', 
+// a.order_time , a.status FROM 
+// `order_all` a , `users` b , `sellers` c , `products` d 
+// where a.id = b.id and a.sellers_id = c.id and a.products_id = d.id; ";
+
+
+
 
 // $sqlUser="SELECT * FROM users ";
 // $stmtUser= $db_host->prepare($sqlUser);
@@ -77,16 +184,6 @@ try{
 // }catch(PDOException $e){
 //     echo $e->getMessage();
 // }
-
-// $sqlUser="SELECT * FROM users ";
-// $stmtUser= $db_host->prepare($sqlUser);
-// try{ 
-//     $stmtUser->execute();
-  
-// }catch(PDOException $e){
-//     echo $e->getMessage();
-// }
-
 
 
 ?>
@@ -182,35 +279,72 @@ try{
         <div class="container pt-5 "> 
                 <h1>訂單總覽</h1>
                 <table class="table  table-striped ttbb">
-                    
+                <?php if (isset($p)) : ?>
+                        <div class="py-2">此頁顯示第<?= $starNo ?>~<?= $starEnd ?>筆
+                        <br>
+                        <br>
+                        共<?= $totalUsersCount ?>筆訂單 </div>
+                    <?php else : ?>
+                        <div class="py-2">
+                            共<?= $totalUsersCount ?>筆訂單
+                        </div>
+                    <?php endif; ?>
                     <thead>
+                        
                             <tr class="">
                                 <th>訂單編號</th>
-                                <th>訂單會員</th>
-                                <th>訂單商家</th>
-                                <th>訂單商品</th>
-                                <th>訂單價錢</th>
+                                <th>會員</th>
+                                <th>商家</th>
+                                <th>商品</th>
+                                <th>價錢</th>
                                 <th>成立時間</th>
                                 <th>訂單狀態</th>
-                                <th></th>
+                                <th>
+                                    <form action="./orders-list.php" method="GET">
+                                        <div class="input-group  search-user">
+                                             <input type="search" class="form-control" placeholder="搜尋訂單編號、會員姓名、商家店名" aria-label="Recipient's username" aria-describedby="button-addon2" name="search" value=<?php if (isset($search)) echo $search ?>>
+                                            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">搜尋</button>
+                                        </div>
+                                    </form>
+                                </th>
                             </tr>
                         <tbody class="">
                         <?php while($rowOrder=$stmtOrder->fetch()):?>
                             <tr class="table-text-all">
                                 <td><?=$rowOrder['id']?></td>
-                                <td><?=$rowOrder['users_id']?></td>
-                                <td><?=$rowOrder['sellers_name']?></td>
+                                <td>
+                                <a class="text-decoration-none text-dark d-flex" href="../user.php?id=<?=$rowOrder['users_id']?>
+                                <?php
+                                if (isset($p)) echo "&p=$p";
+                                if (isset($search)) echo "&search=$search" ?>
+                                "type="submit">
+                                     <?=$rowOrder['user_name']?>
+                                    <img class="magnifier-img" src="../img/search-solid.svg" alt="">
+                                </a>
+                                </td>
+                                <td>
+                                <a class="text-decoration-none text-dark d-flex" href="../sellers/seller.php?id=<?= $rowOrder['seller_id']?>
+                                <?php
+                                if (isset($p)) echo "&p=$p";
+                                if (isset($search)) echo "&search=$search" ?>" type="submit">
+                                  <?=$rowOrder['seller_name']?>
+                                    <img class="magnifier-img" src="../img/search-solid.svg" alt="">
+                                </a>
+                                </td>
+                                   
                                 <td><?=$rowOrder['products_name']?></td>
                                 <td>$<?=$rowOrder['order_price']?></td>
                                 <td><?=$rowOrder['order_time']?></td>
                                 <td>
-                                    <?php if($rowOrder['status'] === "1"){
-                                        echo "已成立";
-                                    }else{
-                                        echo "未成立";}?>
-                                    </td>
+                                    <?php if ($rowOrder['status'] == 1):?> 
+                                        <p class="text-primary">已成立</p>
+                                    <?php else:?>
+                                        <p class="text-text-danger">已成立</p>
+                                    <?php endif?>
+                                </td>
                                 <td> 
-                                    <a href="orders-list.php?id=<?= $rowOrder['id'] ?>" class="btn btn-outline-secondary" type="submit">詳細資訊</a>
+                                    <a href="orders-list.php?id=<?= $rowOrder['id'] ?><?php if (isset($p)) echo "&p=$p";if (isset($search)) echo "&search=$search";?>
+                                    "class="btn btn-outline-secondary" type="submit">詳細資訊</a>
                                 </td>
                             </tr>
                             <?php endwhile;?>
@@ -235,16 +369,41 @@ try{
                     </td>
                 </tr>
                 <tr>
-                    <th>訂單會員</th>
+                    <th>會員ID</th>
                     <td><?= $rowUserUserTable['users_id'] ?></td>
                 </tr>
                 <tr>
-                    <th>會員id</th>
-                    <td><?= $rowUserUserTable['users_id'] ?></td>
+                    <th>會員</th>
+                    <td><?= $rowUserUserTable['user_name'] ?></td>
                 </tr>
                 <tr>
-                    <th>訂單商家</th>
-                    <td><?= $rowUserUserTable['sellers_name'] ?></td>
+                    <th>會員信箱</th>
+                    <td><?= $rowUserUserTable['user_email'] ?></td>
+                </tr>
+                <tr>
+                    <th>會員手機號碼</th>
+                    <td><?= $rowUserUserTable['user_email'] ?></td>
+                </tr>
+               
+                <tr>
+                    <th>商家ID</th>
+                    <td><?= $rowUserUserTable['seller_id'] ?></td>
+                </tr>
+                <tr>
+                    <th>商家</th>
+                    <td><?= $rowUserUserTable['seller_name'] ?></td>
+                </tr>
+                <tr>
+                    <th>商家地址</th>
+                    <td><?= $rowUserUserTable['seller_name'] ?></td>
+                </tr>
+                <tr>
+                    <th>商家信箱</th>
+                    <td><?= $rowUserUserTable['seller_email'] ?></td>
+                </tr>
+                <tr>
+                    <th>商家手機號碼</th>
+                    <td><?= $rowUserUserTable['seller_email'] ?></td>
                 </tr>
                 <tr>
                     <th>訂單商品</th>
@@ -252,30 +411,43 @@ try{
                 </tr>
                 <tr>
                     <th>訂單價錢</th>
-                    <td><?= $rowUserUserTable['order_price'] ?></td>
+                    <td>$<?= $rowUserUserTable['order_price'] ?></td>
                 </tr>
                 <tr>
-                    <th>成立時間</th>
+                    <th>訂單成立時間</th>
                     <td><?= $rowUserUserTable['order_time'] ?></td>
                 </tr>
                 <tr>
                     <th>訂單狀態</th>
-                    <td><?php if ($rowUserUserTable['status'] == 1) {
-                            echo "啟用";
-                        } else {
-                            echo "停用";
-                        }
-
-                        ?></td>
+                    <td><?php if ($rowUserUserTable['status'] == 1):?> 
+                            <p class="text-primary">已成立</p>
+                        <?php else:?>
+                            <p class="text-text-danger">已成立</p>
+                        <?php endif?>
+                        </td>
 
                 </tr>
 
             </table>
         </div>
     <?php endif ?>
+
+
+    <?php if (isset($p)) : ?>
+        <nav aria-label="Page navigation example ">
+            <ul class="pagination  justify-content-center">
+                <?php for ($i = 1; $i <= $pageConet; $i++) : ?>
+                    <li class="page-item page-nav <?php if ($p == $i) echo 'active' ?>">
+                        <a class="page-link" href="http://localhost/spotGoods/admin/admin.php?p=<?= $i ?>"><?= $i ?>
+                        </a>
+                    </li>
+                <?php endfor; ?>
+            </ul>
+        </nav>
+    <?php endif ?>
     </main>
 
-
+ 
 
 
     <!-- 即時客服 -->
