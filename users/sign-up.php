768bab049110fc6d1c0176a0804dd9ccd411e084
chiupao <a0912979737@gmail.com>
2021-12-07 17:17:25 +0800

    忘記有無更動東西，更新一下

--- users/sign-up.php
+++ users/sign-up.php
@@ -15,7 +15,6 @@
             opacity: 1; /* Firefox */
         }
     </style>
-
 </head>
 <body>
 <header class="bg-light">
@@ -42,7 +41,8 @@
                 </div>
                 <div class="mb-3">
                     <label for="email">電子信箱</label>
-                    <input id="email" type="email" name="email" required class="form-control" placeholder="name@example.com">
+                    <input id="email" type="email" name="email" required class="form-control"
+                           placeholder="name@example.com">
                     <div class="error text-danger text-end"></div>
                 </div>
                 <div class="mb-3">
@@ -51,11 +51,13 @@
                 </div>
                 <div class="mb-3">
                     <label for="password">密碼</label>
-                    <input id="password" type="password" name="password" required class="form-control" placeholder="設定您的密碼">
+                    <input id="password" type="password" name="password" required class="form-control"
+                           placeholder="設定您的密碼">
                 </div>
                 <div class="mb-3">
                     <label for="repassword">確認密碼</label>
-                    <input id="repassword" type="password" name="repassword" required class="form-control" placeholder="請再次輸入密碼">
+                    <input id="repassword" type="password" name="repassword" required class="form-control"
+                           placeholder="請再次輸入密碼">
                 </div>
                 <div class="mb-3">
                     <label for="phone">手機號碼</label>
@@ -75,32 +77,34 @@
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
 
 <script>
 
-    const reName=/^%&',;=?$x22]+/;
+    // const reName = /^%&',;=?$x22]+/;
 
     //電子信箱格式檢查
-    const reEmail=/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-    $("#email").blur(function(){
-        if(reEmail.test($("#email").val())){
+    const reEmail = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    $("#email").blur(function () {
+        if (reEmail.test($("#email").val())) {
             $(this).next(".error").text("");
             $(this).removeClass("border-danger");
-        }else{
+        } else {
             $(this).next(".error").text("輸入格式有誤");
             $(this).addClass("border-danger");
         }
     });
 
     //手機號碼格式檢查
-    const rePhone=/^09\d{8}$/;  //09開頭，數字{8次}，$代表字元結尾
-    $("#phone").blur(function(){
-        if(rePhone.test($("#phone").val())){
+    const rePhone = /^09\d{8}$/;  //09開頭，數字{8次}，$代表字元結尾
+    $("#phone").blur(function () {
+        if (rePhone.test($("#phone").val())) {
             // alert("符合規則");
             $(this).next(".error").text("");
             $(this).removeClass("border-danger");
-        }else{
+        } else {
             // alert("不符合規則");
             $(this).next(".error").text("輸入格式有誤");
             $(this).addClass("border-danger");
@@ -108,11 +112,11 @@
     });
 
     //尚未註冊完成，離開頁面提醒
-    $(".navbar-brand").click(function(){
-        let result=confirm("尚未註冊完成，確定要離開此頁面?");
-        if(result){
+    $(".navbar-brand").click(function () {
+        let result = confirm("尚未註冊完成，確定要離開此頁面?");
+        if (result) {
             return true;
-        }else {
+        } else {
             return false;
         }
     });
