7bc305bcbd237cf05d68b8b5280b493ad2518ab8
chang <ch96963@gmail.com>
2021-12-10 15:36:16 +0800

    新增管理員密碼加密 直接寫入

--- admin/doLogin.php
+++ admin/doLogin.php
@@ -12,26 +12,37 @@ if(isset($_POST['account'])||isset($_POST['password'])){
     exit();
 }
 
-$sqlAdmin="SELECT * FROM admin WHERE account=? AND password=?";
+$sqlAdmin="SELECT * FROM admin WHERE account=? ";
 $stmtAdmin=$db_host->prepare($sqlAdmin);
 
 try{
-    $stmtAdmin->execute([$account,$password]);
+    $stmtAdmin->execute([$account]);
     $userExist=$stmtAdmin->rowCount();
   
     if($userExist>0){
         $rowAdmin=$stmtAdmin->fetch();
-        $user=[
-            "id"=>$rowAdmin['id'],
-            "name"=>$rowAdmin['name'],
-            "account"=>$rowAdmin['account'],
-            "password"=>$rowAdmin['email']
-        ];
-        $_SESSION["user"]=$user;
-        header("location: admin.php");
-        unset($_SESSION["error_times"]);
-        unset($_SESSION["error_msg"]);
-        echo $user;
+        if(password_verify($password,$rowAdmin['password'])){
+            $user=[
+                "id"=>$rowAdmin['id'],
+                "name"=>$rowAdmin['name'],
+                "account"=>$rowAdmin['account'],
+                "password"=>$rowAdmin['email']
+            ];
+            $_SESSION["user"]=$user;
+            header("location: admin.php");
+            unset($_SESSION["error_times"]);
+            unset($_SESSION["error_msg"]);
+            echo $user;
+           }else{
+            $_SESSION['error_msg']="帳號或密碼輸入錯誤";
+            if(isset($_SESSION["error_times"])){
+                $_SESSION["error_times"]=$_SESSION["error_times"]+1;
+            }else{
+                $_SESSION["error_times"]=1;
+            }
+            header("location: admin-Login.php");
+            };
+     
     }
     else{
         $_SESSION['error_msg']="帳號或密碼輸入錯誤";
